57cbc3b22f7d95ce98ba0862043cb55c8532482c
patpat <tomaspatrickdanielle@gmail.com>
2026-08-03 20:42:58 +0800

    Make the obstacle move

---
+++ Assets/Scripts/ObstacleMover.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class ObstacleMover : MonoBehaviour
+{
+    public float speed = 3f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += Vector3.left * speed * Time.deltaTime; //delta time to make it frame independent
+    }
+}
