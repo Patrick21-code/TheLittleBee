22cc29a12c7e6db771088d163b1315789c41d3f5
patpat <tomaspatrickdanielle@gmail.com>
2026-08-03 19:13:29 +0800

    Bee Control

---
+++ Assets/Scripts/BeeController.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class BeeController : MonoBehaviour
+{
+    Rigidbody2D rb;
+    public float flapStrength = 5f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovePlayer();
+    }
+
+    void MovePlayer()
+    {
+        if (Mouse.current.leftButton.wasPressedThisFrame)
+        {
+            // Reset vertical velocity for consistent flap
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, 0);
+            
+            // Apply uniform upward force
+            rb.AddForce(Vector2.up * flapStrength, ForceMode2D.Impulse);
+        }
+        
+    }
+}
