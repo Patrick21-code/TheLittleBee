e4fa9235cd8857cec08598cc7e9950cf0e4287a5
patpat <tomaspatrickdanielle@gmail.com>
2026-08-03 21:12:33 +0800

    Spawn the obstacle every 2 seconds

---
+++ Assets/Scripts/ObstacleSpawner.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class ObstacleSpawner : MonoBehaviour
+{
+    //Every Prefab is a GameObject, so we can use GameObject type to store the prefab reference
+    public GameObject obstaclePrefab;
+    public float spawnRate = 2f; //Spawns object every 2 seconds
+    private float timer = 0f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer >= spawnRate)
+        {
+            Instantiate(obstaclePrefab, transform.position, Quaternion.identity); //Quaternion.identity means no rotation
+            timer = 0f;
+        }
+    }
+}
