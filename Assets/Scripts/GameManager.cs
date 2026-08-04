65455e0cba2125f5bfdb75f1d27a4e2df2844d44
patpat <tomaspatrickdanielle@gmail.com>
2026-08-04 18:33:57 +0800

    create GameManager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    //instead of each object having its own gameOver, everyone checks the same one
+    public static bool gameOver = false; //static means there's only one shared value for the entire game
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
