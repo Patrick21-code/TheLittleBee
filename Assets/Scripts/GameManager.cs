6609baf78139516a018868d972987c7134c2e06c
patpat <tomaspatrickdanielle@gmail.com>
2026-08-04 23:21:40 +0800

    add score data

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,12 +4,13 @@ public class GameManager : MonoBehaviour
 {
     //instead of each object having its own gameOver, everyone checks the same one
     public static bool gameOver = false; //static means there's only one shared value for the entire game
-
+    public static int score = 0;
     //reset the game state when a new game starts
     //unity calls Awake before Start on all active objects in the scene
     void Awake ()
     {
         gameOver = false;
+        score = 0;
     }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
