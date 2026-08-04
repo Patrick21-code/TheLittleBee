7d2c6a281bbff385985a83fb3e23780e85251a87
patpat <tomaspatrickdanielle@gmail.com>
2026-08-04 23:24:00 +0800

    Add score zone script

---
+++ Assets/Scripts/ScoreZone.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class ScoreZone : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //Collision2D collision contains information about what you hit
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+    //if the object entering is the Player
+     if (collision.CompareTag("Player"))
+        {
+            GameManager.score++;
+            Debug.Log("Score: " + GameManager.score);
+        }   
+    }
+}
